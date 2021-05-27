1b1fbd304de8e269d36ca70e3a3c3df417c98ef9
sixj <sixj@runlion.com>
2021-05-27 14:06:23 +0800

    add: 用户获取服务器的硬件信息

---
+++ license-server-info/src/main/java/com/sixj/license/LicenseServerInfoApplication.java
@@ -0,0 +1,18 @@
+package com.sixj.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户获取服务器的硬件信息
+ * @author sixiaojie
+ * @date 2021-05-25-13:38
+ */
+@SpringBootApplication
+public class LicenseServerInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LicenseServerInfoApplication.class, args);
+    }
+
+}
---
+++ license-server-info/src/main/java/com/sixj/license/controller/CreatorLicenseController.java
@@ -0,0 +1,46 @@
+package com.sixj.license.controller;
+
+import cn.hutool.system.OsInfo;
+import cn.hutool.system.SystemUtil;
+import com.sixj.license.info.AbstractServerInfo;
+import com.sixj.license.info.LinuxServerInfo;
+import com.sixj.license.info.MacOsServerInfo;
+import com.sixj.license.info.WindowsServerInfo;
+import com.sixj.license.model.LicenseCheckModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 生成证书的Controller
+ * @author sixiaojie
+ * @date 2021-05-25-14:01
+ */
+@RestController
+@RequestMapping("/license")
+public class CreatorLicenseController {
+
+
+    /**
+     * 获取服务器硬件信息
+     * @return
+     */
+    @GetMapping(value = "/getServerInfo")
+    public LicenseCheckModel getServerInfo() {
+
+        AbstractServerInfo abstractServerInfo;
+
+        //根据不同操作系统类型选择不同的数据获取方法
+        OsInfo osInfo = SystemUtil.getOsInfo();
+        if (osInfo.isWindows()) {
+            abstractServerInfo = new WindowsServerInfo();
+        } else if (osInfo.isMac()) {
+            abstractServerInfo = new MacOsServerInfo();
+        }else{//其他服务器类型
+            abstractServerInfo = new LinuxServerInfo();
+        }
+
+        return abstractServerInfo.getServerInfos();
+    }
+}
